1875a47274e7980bf3e072cb8cad2593c03b2238
MegaBluejay <dmoiseev2011@gmail.com>
2020-06-16 23:14:14 +0300

    remove setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name='jaytris',
-    version='0.1',
-    author='MegaBluejay',
-    description='Tetris implementation meeting most, but not all, of the guidelines',
-    
-)
